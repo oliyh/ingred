d2d2ab179849a7d2af903aac71c819756f720703
oliyh <git@oliy.co.uk>
2013-09-15 13:32:23 -0700

    preparing for heroku deployment

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/oliyh/ingred"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.2"]
                  [ring "1.2.0"]
