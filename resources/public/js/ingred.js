df2b381ade418ced0618bb6cc5dc12574f28719d
oliyh <git@oliy.co.uk>
2013-09-15 11:09:47 -0700

    improving demo to show recipe details

--- resources/public/js/ingred.js
+++ resources/public/js/ingred.js
@@ -14,8 +14,34 @@ function populateRecipes() {
 	$.each(data, function(i, e) {
 	    var recipe = $('<li/>')
 		.append($('<a/>', {class: 'label label-warning', href: e.url, title: e.name})
-			.text(e.name));
+			.text(e.name)
+			.click(function() { loadRecipe(e.url); return false; }));
 	    $('#letters').append(recipe);
+	});
+    });
+}
+
+function loadRecipe(url) {
+    $.get(url, function (recipe) {
+	$('#recipe-name').text(recipe.name);
+	$('#recipe-prep-time').text(recipe['preparation-time']);
+	$('#recipe-cooking-time').text(recipe['cooking-time']);
+	$('#recipe-yield').text(recipe.yield);
+
+	$('#recipe-ingredients').empty();
+	$.each(recipe.ingredients, function(i, e) {
+	    var ingredient = $('<li/>')
+		.append(e.qty)
+		.append($('<a/>', {class: 'label label-success', href: e.url, title: e.name})
+			.text(e.name))
+		.append(e.preparation);
+	    $('#recipe-ingredients').append(ingredient);
+	});
+
+	$('#recipe-instructions').empty();
+	$.each(recipe.instructions, function(i, e) {
+	    var ingredient = $('<li/>').text(e)
+	    $('#recipe-instructions').append(ingredient);
 	});
     });
 }
