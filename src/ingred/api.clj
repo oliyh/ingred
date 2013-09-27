63b5e35b8182f4c0fdb5c02eb2511dfca6eecd8f
oliyh <git@oliy.co.uk>
2013-09-27 12:34:27 -0700

    renaming fn

--- src/ingred/api.clj
+++ src/ingred/api.clj
@@ -7,15 +7,15 @@
 (defn index []
   (slurp "resources/public/index.html"))
 
-(defn- url-and-name [recipes]
+(defn- recipe-summary [recipes]
   (map (fn [recipe] {:name (:name recipe) :url (str "/recipes/" (:id recipe))}) recipes))
 
 (defn list-recipes
-  ([] (response (url-and-name (store/list-all))))
-  ([letter] (response (url-and-name (store/by-letter letter)))))
+  ([] (response (recipe-summary (store/list-all))))
+  ([letter] (response (recipe-summary (store/by-letter letter)))))
 
 (defn list-recipes-for [ingredient]
-  (response (url-and-name (store/by-ingredient ingredient))))
+  (response (recipe-summary (store/by-ingredient ingredient))))
 
 (defn list-ingredients []
   (response (into #{} (map #(assoc % :url (str "/ingredients/" (:name %))) (store/list-ingredients)))))
